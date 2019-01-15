c5cd33389038ca96fcc6e427aea7cd94095ec6a2
SaSaChu <teresa7397@gmail.com>
2019-01-15 09:44:53 +0800

    新增鎖定頁面

---
+++ js/190115.js
@@ -0,0 +1,10 @@
+
+function openBlackMark(){
+  $('body').addClass('blackMark');
+  $('.blackmark').removeClass('off');
+}
+
+function offBlackMark(){
+  $('body').removeClass('blackMark');
+  $('.blackmark').addClass('off');
+}
